0e1c0371f7261e08a283d8641335096e905a3b1e
Mateus Santana <mateus-sgn@hotmail.com>
2017-09-03 12:21:48 -0300

    Primeiro commit

---
+++ Equacoes1o2oGraus/Equacoes1o2oGraus/Equacoes1o2oGraus.c
@@ -0,0 +1,68 @@
+/*#include <stdio.h>
+#include <math.h>
+
+void resolve1oGrau();
+void resolve2oGrau();
+float valida(float);
+void menuSistema();
+
+int main() {
+	int opcao;
+	do {
+		menuSistema();
+		scanf_s("%d", &opcao);
+		switch (opcao) {
+		case 1: resolve1oGrau();
+			break;
+		case 2: resolve2oGrau();
+			break;
+		case 3: printf("Hasta la vista, baby!!!\n");
+			break;
+		default: printf("Opcao invalid. Preste atencao!!!\n");
+		}
+	} while (opcao != 3);
+	return 0;
+}
+
+void resolve1oGrau() {
+	float a, b;
+	a = valida(a);
+	printf("Digite coeficiente b: ");
+	scanf_s("%f", &b);
+	printf("Raiz = %f\n", -b / a);
+}
+
+void menuSistema() {
+	printf("1........Resolve equacao 1o grau\n");
+	printf("2........Resolve equacao 2o grau\n");
+	printf("3........Sair\n");
+	printf("\n\n\nOpcao: ");
+}
+
+void resolve2oGrau() {
+	float a, b, c, delta;
+	a = valida(a);
+	printf("Digite coeficiente b: ");
+	scanf_s("%f", &b);
+	printf("Digite coeficiente c: ");
+	scanf_s("%f", &c);
+	delta = b*b - 4 * a*c;
+	if (delta >= 0) { // As raizes sao reais. Ainda nao sei se apenas 1 ou 2 raizes...
+		if (delta>0)
+			printf("Raiz1=%f,\nRaiz2=%f\n", (-b - sqrt(delta)) / (2 * a),
+			(-b + sqrt(delta)) / (2 * a));
+		else
+			printf("Raiz unica=%f\n", -b / (2 * a));
+	}
+}
+
+float valida(float c_a) {
+	printf("Digite a (a!=0): ");
+	scanf_s("%f", &c_a);
+	while (c_a == 0) {
+		printf("Ooops. a deve ser != 0. Digite novamente: ");
+		scanf_s("%f", &c_a);
+	}
+	return c_a;
+}
+*/
---
+++ Equacoes1o2oGraus/Equacoes1o2oGraus/QuantosZerosTem.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+int fat(int numero);
+int quantosZerosTem(int resultadoFat);
+int numeroValido();
+
+int main() {
+	int escolha;
+	do {
+		printf("1 - Ver quantos zeros tem \n");
+		printf("2 - Sair \n");
+		scanf_s("%d", &escolha);
+		switch (escolha) {
+		case 1: quantosZerosTem(fat(numeroValido()));
+			break;
+		}
+	} while (escolha != 2);
+}
+int numeroValido() {
+	int numero;
+	do {
+		printf("Digite um numero entre 1 e 100.000: \n");
+		scanf_s("%d", &numero);
+		if (numero < 1 || numero > 100000) {
+			printf("Numero invalido\n");
+		}
+	} while (numero < 1 || numero > 100000);
+	return numero;
+}
+int fat(int numero) {
+	int resultadoFat = numero;
+	while(numero > 1) {
+		numero--;
+		resultadoFat = resultadoFat * numero;
+	}
+	printf("O fatorial e %d \n", resultadoFat);
+	return resultadoFat;
+}
+int quantosZerosTem(int resultadoFat) {
+	int numeroDeZeros = 0;
+	while (resultadoFat % 10 == 0) {
+		numeroDeZeros++;
+		resultadoFat = resultadoFat / 10;
+	}
+	printf("Tem %d zero(s)! \n", numeroDeZeros);
+}
