4b410354b7367bd1f4177cd642254d57193f4ad0
Mateus Santana <mateus-sgn@hotmail.com>
2017-09-08 11:27:54 -0300

    Atualizando 3n+1

--- 3n+1/3n+1/3n+1.c
+++ 3n+1/3n+1/3n+1.c
@@ -1,32 +1,45 @@
 #include <stdio.h>
 
 int main() {
-	int numero1;
-	int numero2;
+	int numero1, numero2, numero3, numero4, numero5, numero6, numero7, numero8;
+
+	printf("Digite dois numeros: \n");
+	scanf_s("%d %d", &numero1, &numero2);
+	scanf_s("%d %d", &numero3, &numero4);
+	scanf_s("%d %d", &numero5, &numero6);
+	scanf_s("%d %d", &numero7, &numero8);
+
+	cicloMaximo(numero1, numero2);
+	cicloMaximo(numero3, numero4);
+	cicloMaximo(numero5, numero6);
+	cicloMaximo(numero7, numero8);
+	
+	system("pause");
+	return 0;
+}
+int cicloMaximo(int primeiroNumero, int segundoNumero) {
+	int cicloAtual, resultado;
 	int maior = 0;
 	int menor = 0;
 	int cicloAnterior = 0;
-	int cicloAtual;
-	int resultado;
-	printf("Digite dois numeros: \n");
-	scanf_s("%d %d", &numero1, &numero2);
-	if (numero1 > numero2) {
-		maior = numero1;
-		menor = numero2;
+
+	if (primeiroNumero > segundoNumero) {
+		maior = primeiroNumero;
+		menor = segundoNumero;
 	}
 	else {
-		maior = numero2;
-		menor = numero1;
+		maior = segundoNumero;
+		menor = primeiroNumero;
 	}
 	while (menor <= maior) {
 		cicloAtual = 1;
-		resultado = 0;
-		while (resultado != 1)
-		{
-			if (menor % 2 == 0) {
-				resultado = menor / 2;
-			} else {
-				resultado = (menor * 3) + 1;
+		resultado = menor;
+		while (resultado != 1) {
+			if ((resultado % 2) == 0) {
+				resultado /= 2;
+			}
+			else {
+				resultado = (resultado * 3) + 1;
 			}
 			cicloAtual++;
 		}
@@ -35,7 +48,6 @@ int main() {
 		}
 		menor++;
 	}
-	printf("%d %d %d", numero1, numero2, cicloAnterior);
-	system("pause");
+	printf("%d %d %d \n", primeiroNumero, segundoNumero, cicloAnterior);
 	return 0;
 }
