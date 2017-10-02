98acf90ee012d17f05dae1d271a4baafd2b18bf8
Mateus Santana <mateus-sgn@hotmail.com>
2017-10-02 15:50:26 -0300

    Os iguais não tem vez
    Problemas corrigidos e programa rodando normalmente

--- Os iguais não tem vez/Os iguais não tem vez/Os iguais não tem vez.c
+++ Os iguais não tem vez/Os iguais não tem vez/Os iguais não tem vez.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 void ordenando_vetor(int vetor[], int tamanho_vetor);
+void imprime_vetor(int vetor[], int tamanho_vetor);
 
 int main() {
 	int quantidade_de_numeros = 0, i = 0;
@@ -9,31 +10,38 @@ int main() {
 	printf("Quantos numeros serao digitados? ");
 	scanf_s("%d", &quantidade_de_numeros);
 	printf("Agora digite os numeros: ");
-
 	//LENDO OS NUMEROS
 	for (i = 0; i < quantidade_de_numeros; i++) {
 		scanf_s("%d", &numeros_digitados[i]);
 	}
 	//ORDENANDO O VETOR
-	ordenando_vetor(numeros_digitados[51], quantidade_de_numeros);
-
-	//IMPRIMINDO OS NUMEROS
-	for (i = 0; i < quantidade_de_numeros; i++) {
-		printf("%d ", numeros_digitados[i]);
-	}
+	ordenando_vetor(numeros_digitados, quantidade_de_numeros);
+	//IMPRIMINDO VETOR
+	imprime_vetor(numeros_digitados, quantidade_de_numeros);
+	
 	system("pause");
 	return 0;
 }
 //ORDENANDO OS NUMEROS
 void ordenando_vetor(int vetor[], int tamanho_vetor) {
 	int i, j, ordena;
-	for (i = 0; i < tamanho_vetor - 1; i++) {
-		for (j = 0; j < tamanho_vetor - 1; j++) {
-			if (vetor[i] < vetor[j]) {
-				ordena = vetor[i];
-				vetor[i] = vetor[j];
-				vetor[j] = ordena;
-			}
+	for (i = 1; i < tamanho_vetor; i++) {
+		ordena = vetor[i];
+		for (j = i - 1; (j >= 0) && (ordena < vetor[j]); j--) {
+			vetor[j + 1] = vetor[j];
+		}
+		vetor[j + 1] = ordena;
+	}
+}
+//IMPRIMINDO OS NUMEROS
+void imprime_vetor(int vetor[], int tamanho_vetor) {
+	int i;
+	for (i = 0; i < tamanho_vetor; i++) {
+		if (vetor[i] != vetor[i - 1]) {
+			if (i == tamanho_vetor - 1)
+				printf("%d \n", vetor[i]);
+			else
+				printf("%d ", vetor[i]);
 		}
 	}
 }
