aca5bbbc571e626586f77c4e8ac231344c761680
Mateus Santana <mateus-sgn@hotmail.com>
2017-10-09 16:03:40 -0300

    atualizando

--- Ponteiros/Ponteiros/Ponteiros.c
+++ Ponteiros/Ponteiros/Ponteiros.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
-int maisn() {
-
+int main() {
+	int x = 2;
+	int *ponteiro = &x;
+	printf("x(main) = %d \n", x);
+	*ponteiro = 5;
+	printf("End. x = %d \n", x);
+	getchar();
+	return 0;
 }
